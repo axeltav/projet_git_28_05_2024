d10d18ceb8571ea86464a613f3ad071b64014884
Axel Tavernet <axeltavernet@gmail.com>
2024-05-27 16:46:13 +0200

    add a HTML skeleton to he index page to fix the bug

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<h1>Projet git</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+</body>
+</html><h1>Projet git</h1>
 <h2>Présentation</h2>
 <p>
     Texte de présentation.
@@ -7,3 +16,5 @@
 <footer>
     <a href="mailto:axeltavernet@gmail.com">axeltavernet@gmail.com</a>
 </footer>
+</body>
+</html>x
